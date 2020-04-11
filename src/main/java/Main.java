6ac8ab8ce5dcf199cce7cd3e9da80b80e1d65ead
UnGu Zartex <yentl.kinoo@student.kuleuven.be>
2020-04-11 12:47:36 +0200

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+//TODO DELETE DEZE CLASS
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,5 @@
+import static org.junit.jupiter.api.Assertions.*;
+//TODO DELETE
+class MainTest {
+
+}
